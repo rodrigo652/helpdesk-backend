a67910712a58e67ad0409e68cf7bb7a37af6d34c
rodrigo652 <rodrigo.camargo52@outlook.com>
2024-06-26 13:53:04 -0300

    delete para tecnico implementado

--- src/main/java/org/projeto/helpdesk/services/TecnicoService.java
+++ src/main/java/org/projeto/helpdesk/services/TecnicoService.java
@@ -45,7 +45,14 @@ public class TecnicoService {
         validaPorCpfEEmail(objDTO);
         oldObj = new Tecnico(objDTO);
         return repository.save(oldObj);
+    }
 
+    public void delete(Integer id) {
+        Tecnico obj = findById(id);
+        if (obj.getChamados().size() > 0) {
+            throw new DataIntegrityViolationException("Técnico possui ordens de serviço e não pode ser deletado!");
+        }
+        repository.deleteById(id);
     }
 
     private void validaPorCpfEEmail(TecnicoDTO objDTO) {
