1041bae716f80491fb56f40a2c4fe66b003d2276
rodrigo652 <rodrigo.camargo52@outlook.com>
2024-06-26 14:44:49 -0300

    crud para cliente implementado

--- src/main/java/org/projeto/helpdesk/domain/Cliente.java
+++ src/main/java/org/projeto/helpdesk/domain/Cliente.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import org.projeto.helpdesk.domain.dtos.ClienteDTO;
+import org.projeto.helpdesk.domain.dtos.TecnicoDTO;
 import org.projeto.helpdesk.domain.enums.Perfil;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente extends Pessoa implements Serializable {
@@ -25,6 +28,15 @@ public class Cliente extends Pessoa implements Serializable {
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
         addPerfil(Perfil.CLIENTE);
+    }
+    public Cliente(ClienteDTO obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
     }
 
     public List<Chamado> getChamados() {
